40af4bf812a1f83aac3029f4ea90bd51e5272553
unknown <game.oh.vn@gmail.com>
2025-04-02 12:19:46 +0700

    f

--- src/SuccessTrait.php
+++ src/SuccessTrait.php
@@ -8,18 +8,21 @@ use Hanoivip\Shop\Facades\OrderFacade;
 
 trait SuccessTrait {
     function onSuccess($transId, $amount) {
-        $realAmount = $amount / 100;
-        // save
         $record = PaytrTransaction::where('trans', $transId)->first();
-        $record->amount = $realAmount;
-        $record->status = DirectMethod::STATUS_SUCCESS;
-        $record->save();
-        // event here
-        event(new TransactionUpdated($transId));
-        // TODO: shop need statistic too
-        // TODO: move this to payment
-        $order = $record->transaction->order;
-        $orderDetail = OrderFacade::detail($order);
-        event(new UserTopup($orderDetail->user_id, 0, $realAmount, $transId));
+        if (!empty($record) && $record->status == DirectMethod::STATUS_PENDING)
+        {
+            $realAmount = $amount / 100;
+            // save
+            $record->amount = $realAmount;
+            $record->status = DirectMethod::STATUS_SUCCESS;
+            $record->save();
+            // event here
+            event(new TransactionUpdated($transId));
+            // TODO: shop need statistic too
+            // TODO: move this to payment
+            $order = $record->transaction->order;
+            $orderDetail = OrderFacade::detail($order);
+            event(new UserTopup($orderDetail->user_id, 0, $realAmount, $transId));
+        }
     }
 }
